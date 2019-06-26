dabf1760a76a4f380abbcc3e38cf2cc5f38963f2
Bhaskar Gyan Vardhan <bhaskar.vardhan@walmartlabs.com>
2019-06-26 20:30:21 +0530

    RN 0.59.0 patch added

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,21 +5,20 @@ source $(dirname $0)/env.sh
 # [0] Patch React Native source
 ######################################################################################
 
-# rm -rf $BUILD_DIR
-# git clone --depth=1 --branch ${RN_VERSION} https://github.com/facebook/react-native.git $BUILD_DIR
+rm -rf $BUILD_DIR
+git clone --depth=1 --branch ${RN_VERSION} https://github.com/facebook/react-native.git $BUILD_DIR
 
 PATCHSET=(
   # Patch React Native build to support v8runtime
-  "build_with_v8.patch"
+  "react-native+0.59.9.patch"
 )
 
 cp -Rf $ROOT_DIR/v8runtime $BUILD_DIR/ReactCommon/jsi/
-
-# for patch in "${PATCHSET[@]}"
-# do
-#     printf "### Patch set: $patch\n"
-#     patch -d $BUILD_DIR -p1 < $PATCHES_DIR/$patch
-# done
+for patch in "${PATCHSET[@]}"
+do
+    printf "### Patch set: $patch\n"
+    patch -d $BUILD_DIR -p1 < $PATCHES_DIR/$patch
+done
 
 ######################################################################################
 # [1] Build
